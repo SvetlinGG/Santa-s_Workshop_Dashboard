6ab4d68ad8372abf90e773e772f758f08018b083
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-21 22:47:06 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+import Navigation from './Navigation';
+
+export default function Header(){
+    return (
+        <header className="header">
+            <h1>🎅 Santa\s Workshop Dashboard</h1>
+            <Navigation />
+        </header>
+    );
+}
