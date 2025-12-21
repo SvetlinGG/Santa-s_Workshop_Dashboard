5417aee98ed578ff0a91e0507622d6cd642ab5ab
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-21 22:51:51 +0100

    footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+
+    return (
+        <footer className="footer">
+            <p>© {new Date().getFullYear()} Santa’s Workshop</p>
+        </footer>
+
+    );
+}
