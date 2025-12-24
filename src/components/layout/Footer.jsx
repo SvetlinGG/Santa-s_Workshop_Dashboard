763da62356622937823230fd9c084793aba1dc1b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-24 08:09:42 +0200

    setup

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
 
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear()} Santa’s Workshop</p>
+            <p>© {new Date().getFullYear()} Santa’s Workshop - created by Svetlin Garabedyan</p>
         </footer>
 
     );
