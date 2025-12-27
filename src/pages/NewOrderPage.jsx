cbb983ac75c1e3501dfcaec85c386e844965e3ae
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-27 14:35:52 +0200

    new order page

---
+++ src/pages/NewOrderPage.jsx
@@ -0,0 +1,10 @@
+
+export default function NewOrderPage(){
+
+    return (
+        <div className="page">
+            <h2>Create Order</h2>
+            <p>Coming next: form + validation + redirect.</p>
+        </div>
+    );
+}
