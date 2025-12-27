71a38f03b2b0d7bd51872ca76307884be08c79d7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-27 17:06:12 +0200

    new routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from "./components/layout/Layout"
-import HomePage from "./pages/HomePage"
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="toys" element={<div className="page">Toys Page</div>} />
-        <Route path="orders" element={<div className="page">Orders Page</div>} />
-        <Route path="elves" element={<div className="page">Elves Page</div>} />
-      </Route>
-    </Routes>
-  )
+
+export default function App() {
+  return <AppRoutes />
 }
 
-export default App
+ 
