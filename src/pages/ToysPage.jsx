f6dd5d84df1e5e6ff46a2e6284f979d59a033448
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-27 11:52:55 +0200

    toys page

---
+++ src/pages/ToysPage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function ToysPage(){
+    return (
+        <div className="page">
+            <h2>Toys</h2>
+            <p>Coming next: list + filters + sorting.</p>
+        </div>
+    );
+}
