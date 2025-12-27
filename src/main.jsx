4f51a789c507d78a9e9e5bc09da6e3d07ba2ab36
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-27 17:24:18 +0200

    main section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { createRoot } from 'react-dom/client'
+
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/globals.css'
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import {queryClient} from "./queryClient";
+import { QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 )
