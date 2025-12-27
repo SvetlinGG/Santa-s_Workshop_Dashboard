b4a350ac5c44e39424995fff7b6002a65be992bd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-27 17:04:01 +0200

    routin to the other pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import HomePage from "../pages/HomePage";
-import ToyPage from "../pages/ToysPage"
+import ToyPage from "../pages/ToysPage";
+import ToyDetailsPage from "../pages/ToyDetailsPage";
+import OrdersPage from "../pages/OrdersPage";
+import NewOrderPage from "../pages/NewOrderPage";
+import ElvesPage from "../pages/ElvesPage";
+import ElfProfilePage from "../pages/ElfProfilePage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 
 export default function AppRoutes(){
@@ -13,6 +19,14 @@ export default function AppRoutes(){
 
             <Route path="/toys" element={<ToyPage />} />
             <Route path="/toys/:toyId" element={<ToyDetailsPage />} />
+
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+
+            <Route path="/elves" element={<ElvesPage />} />
+            <Route path="/elves/:elfId/*" element={<ElfProfilePage />} />
+
+            <Route path="*" element={<NotFoundPage />} />
             </Route>
         </Routes>
     )
