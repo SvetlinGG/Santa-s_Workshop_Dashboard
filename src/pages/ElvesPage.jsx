fd72e3355dcf2d881fc2ce3431484b367e1ea689
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-30 01:01:54 +0200

    update elves page

--- src/pages/ElvesPage.jsx
+++ src/pages/ElvesPage.jsx
@@ -1,10 +1,25 @@
+import { useQuery }  from "@tanstack/react-query";
+import { elvesApi } from "../api/elves.api";
+
+import Loader from '../components/common/Loader';
+import ErrorFallback from '../components/common/ErrorFallback';
+import ElvesList from '../components/elves/ElvesList';
+
 
 export default function ElvesPage(){
+
+    const { data: elves = [], isLoading, isError, error } = useQuery({
+        queryKey: ['elves'],
+        queryFn: elvesApi.getAll,
+    });
+
+    if ( isLoading ) return <Loader text="Loading elves..." />
+    if ( isError ) return <ErrorFallback title="Failed to load elves" error={error} />
 
     return (
         <div className="page">
             <h2>Elves</h2>
-            <p>Coming next: list + profile + tasks nested route.</p>
+            <ElvesList elves={elves} />
         </div>
     );
 }
