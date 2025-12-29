9fe7a580c6130977b96accd7f9ae36314dd82c1d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-30 01:26:25 +0200

    update elf profile page component

--- src/pages/ElfProfilePage.jsx
+++ src/pages/ElfProfilePage.jsx
@@ -1,24 +1,50 @@
-import { NavLink, Route, Routes, useParams } from "react-router-dom";
+import { Link, NavLink, Route, Routes, useParams } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query';
 
-function TasksPlaceholder(){
-    return <p>Coming next: tasks list (nested route)</p>
-}
+import { elvesApi } from '../api/elves.api';
+
+import Loader from '../components/common/Loader';
+import ErrorFallback from '../components/common/ErrorFallback';
+
+import ElfProfile from '../components/elves/ElfProfile';
+import ElfTasks from '../components/elves/ElfTasks';
+
+
+
 
 export default function ElfProfilePage(){
     const { elfId } = useParams();
 
+    const { data: elf, isLoading, isError, error } = useQuery({
+        queryKey: ["elves", elfId],
+        queryFn: () => elvesApi.getById(elfId),
+    });
+
+    if (isLoading) return <Loader text="Loading elf profile..." />;
+    if (isError) return <ErrorFallback title="Failed to load elf" error={error} />;
+    if (!elf) return <ErrorFallback title="Elf not found" error={{ message: "no data"}} />;
+
     return (
         <div className="page">
-            <h2>Elf Profile</h2>
-            <p>Elf ID: {elfId}</p>
-            <div style={{ display: 'flex', gap: '0.75rem', margin: '0.75rem 0'}}>
-                <NavLink to="" end>Profile</NavLink>
-                <NavLink to="tasks">Tasks</NavLink>
+            <div style={{ marginBottom: '0.75rem'}}>
+                <Link to='/elves' className="link">
+                ← Back to Elves
+                </Link>
             </div>
 
+            <h2>Elf</h2>
+
+            <div className="tabs" style={{ marginTop: '0.5rem'}}>
+                <NavLink className={({ isActive}) => `tab ${isActive ? 'active' : ''}`} to='end'>
+                    Profile
+                </NavLink>
+                <NavLink className={({ isActive}) => `tab ${isActive ? 'active' : ''}`} to='tasks'>
+                    Tasks
+                </NavLink>
+            </div>
             <Routes>
-                <Route index element={<p>Coming next: profile + boost energy button.</p>} />
-                <Route path="tasks" element={<TasksPlaceholder />} />
+                <Route index element={<ElfProfile elf={elf} />} />
+                <Route path="tasks" element={<ElfTasks tasks={elf.tasks || []} />} />
             </Routes>
         </div>
     )
